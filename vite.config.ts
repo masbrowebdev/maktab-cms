64c0e4b1dbad332936f3380af08a406072ba2904
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2025-02-23 12:31:28 +0700

    setup tailwind 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+// import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      // plugins: [tailwindcss, autoprefixer],
+      plugins: [autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths(), netlifyPlugin()],
+  plugins: [reactRouter(), tsconfigPaths(), netlifyPlugin(), tailwindcss()],
 });
